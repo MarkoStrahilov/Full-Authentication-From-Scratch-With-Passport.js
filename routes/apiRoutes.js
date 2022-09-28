68969b38c96535de74483e8076023116fbdbb904
Marko Strahilov <strahilovmarko@yahoo.com>
2022-09-28 17:30:03 +0200

    convert to full api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,27 +5,14 @@ const router = express.Router({ mergeParams: true });
 const { asyncErrorHandle } = require('../middleware')
 
 // auth functions
-const { getRegistration, getSignIn, successPage } = require('../controllers/authRender')
-
 const { register, validateToken } = require('../controllers/register')
 const { signIn } = require('../controllers/signIn')
 
-router.get('/register', asyncErrorHandle(getRegistration))
-
-router.get('/sign-in', asyncErrorHandle(getSignIn))
-
-router.get('/success', asyncErrorHandle(successPage))
-
 // registration
-
 router.post('/api/v1/register/user', asyncErrorHandle(register))
-
 router.get('/register/new/user', asyncErrorHandle(validateToken))
 
-
-
 // sign in
-
 router.post('/api/v1/sign-in/user', asyncErrorHandle(signIn))
 
 module.exports = router
