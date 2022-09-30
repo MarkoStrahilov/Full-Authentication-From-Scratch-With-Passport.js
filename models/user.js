a6d0c8598a67ed398581ac24cf6883a77faa0720
Marko Strahilov <strahilovmarko@yahoo.com>
2022-09-30 17:02:52 +0200

    delete test function method

--- models/user.js
+++ models/user.js
@@ -31,20 +31,5 @@ userSchema.methods.createPasswordResetToken = async function() {
     return otp
 }
 
-// userSchema.methods.testBcryptPasswordResetToken = async function() {
-
-//     const otp = `${Math.floor(Math.random() * 1000000000000000)}`
-//     const hashedOtp = await bcrypt.hash(otp, 12)
-//     const token = hashedOtp.toString()
-
-//     // this.passwordResetToken = token
-//     // this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-
-//     const data = { token, otp }
-
-//     return data
-// }
-
-
 const User = mongoose.model('User', userSchema)
 module.exports = User
