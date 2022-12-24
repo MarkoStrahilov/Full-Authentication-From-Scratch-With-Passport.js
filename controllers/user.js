29d8e29c04717729baa3acff1a59fb327741c69f
Marko Strahilov <strahilovmarko@yahoo.com>
2022-12-24 21:08:02 +0100

    delete user route

---
+++ controllers/user.js
@@ -0,0 +1,36 @@
+const User = require('../models/user')
+
+module.exports.signIn = async(req, res) => {
+    try {
+
+        const foundUser = await User.findOne({ _id: req.query.id });
+
+        if (foundUser.isVerified === false) {
+
+            return res.status(400).send({
+                status: 'fail',
+                message: "account is not verified, please verify your account to continue with this action"
+            })
+
+        }
+
+        const findAndDelete = await User.findOneAndDelete({ _id: foundUser._id })
+
+        if (findAndDelete) {
+
+            return res.status(200).send({
+                status: 'success',
+                message: "account was successfuly deleted"
+            })
+
+        }
+
+    } catch (error) {
+
+        return res.status(400).send({
+            status: 'fail',
+            message: error.message,
+        });
+
+    }
+}
